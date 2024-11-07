7cac446b873d01f93ed908215fa0af3be0fbfc25
silentDoc <ramon78t@yahoo.es>
2024-11-08 00:25:38 +0100

    Day 1 - Part 1 Completed. Finding numbers

---
+++ Day01/ReportFinder.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC20.Day01
+{
+    internal class ReportFinder
+    {
+        List<int> elements = new List<int>();
+        public void ParseInput(List<string> input)
+            => elements = input.Select(x => int.Parse(x)).ToList();
+
+        int SolvePart1()
+        {
+            var x = elements.First(x => elements.Any(y => y + x == 2020 && y != x));
+            return x * (2020 - x);
+        }
+
+        public int Solve(int part = 1)
+            => SolvePart1();
+
+    }
+}
