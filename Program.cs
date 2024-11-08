56816222049cdcbdd25a2311d26e53bd6aadc30b
silentDoc <ramon78t@yahoo.es>
2024-11-08 09:47:42 +0100

    Day 4 - Readme, main and input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC20
     {
         static void Main(string[] args)
         {
-            int day = 3;
-            int part = 2;
-            bool test = false;
+            int day = 4;
+            int part = 1;
+            bool test = !false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -22,6 +22,7 @@ namespace AoC20
                 1 => day1(input, part),
                 2 => day2(input, part),
                 3 => day3(input, part),
+                4 => day4(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -53,5 +54,10 @@ namespace AoC20
             return sled.Solve(part).ToString();
         }
 
+        static string day4(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return "";
+        }
     }
 }
