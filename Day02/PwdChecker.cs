10f5cf6123fb8553a2360be73705ec5fc5ade7d8
silentDoc <ramon78t@yahoo.es>
2024-11-08 08:16:17 +0100

    Day 2 - Part 1 Completed. Counting letters

---
+++ Day02/PwdChecker.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC20.Day02
+{
+    record Element
+    {
+        public int Low = 0; 
+        public int High =0;
+        public char Letter = ' ';
+        public string Pwd = "";
+
+        public Element(string inputLine)
+        {
+            Regex rg = new Regex(@"(\d+)-(\d+) ([a-z]): ([a-z]+)");
+            var groups = rg.Match(inputLine).Groups;
+            Low = int.Parse(groups[1].Value);
+            High = int.Parse(groups[2].Value);
+            Letter = char.Parse(groups[3].Value);
+            Pwd = groups[4].Value;
+        }
+
+        public bool CompliesPolicy
+            => Low <= Pwd.Count(x => x==Letter) && High >= Pwd.Count(y => y==Letter);
+    }
+
+    internal class PwdChecker
+    {
+        List<Element> policies = new List<Element>();
+        
+        public void ParseInput(List<string> lines)
+            => lines.ForEach(x => policies.Add(new Element(x)));
+
+        public int Solve(int part = 1)
+            => policies.Count(x => x.CompliesPolicy);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AoC20
         {
             int day = 2;
             int part = 1;
-            bool test = !false;
+            bool test = false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -39,8 +39,9 @@ namespace AoC20
         static string day2(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-
-            return "";
+            Day02.PwdChecker checker = new();
+            checker.ParseInput(lines);
+            return checker.Solve(part).ToString();
         }
 
     }
