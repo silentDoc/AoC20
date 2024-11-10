8418a1044dc76a3794380e52ca1b0af50a5490df
silentDoc <ramon78t@yahoo.es>
2024-11-10 22:15:50 +0100

    Day 9 - Part 1 Completed. Preambles

---
+++ Day09/XmasEncoder.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC20.Day09
+{
+    internal class XmasEncoder
+    {
+        List<long> numbers = new();
+        int pre = 25;
+        public void ParseInput(List<string> input)
+            => numbers = input.Select(long.Parse).ToList();
+
+        bool Check(int position)
+        {
+            var preamble = numbers[(position - pre)..position];
+            return preamble.Any(x => preamble.Contains(numbers[position] - x) && x!= numbers[position]/2);
+        }
+
+        long SolvePart1()
+        {
+            for (int i = pre; i < numbers.Count; i++)
+                if (!Check(i))
+                    return numbers[i];
+            return -1;
+        }
+
+        public long Solve(int part = 1)
+            => SolvePart1();
+    }
+}
