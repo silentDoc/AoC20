8e14b1aabd8c8e294d1b133fb86d5031bfb3c427
silentDoc <ramon78t@yahoo.es>
2024-11-11 10:38:59 +0100

    Day 10 - Part 1 Completed. Connecting stuff

---
+++ Day10/JoltConnector.cs
@@ -0,0 +1,24 @@
+﻿using AoC20.Common; // List utils
+
+namespace AoC20.Day10
+{
+    internal class JoltConnector
+    {
+        List<int> connectors = [];
+
+        public void ParseInput(List<string> lines)
+            => connectors = lines.Select(int.Parse).ToList();
+
+        int SolvePart1()
+        {
+            var dict = connectors.Prepend(0).Append(connectors.Max() + 3)
+                                 .OrderBy(x => x)
+                                 .Windowed(2)
+                                 .GroupBy(x => x[1] - x[0]).ToDictionary(x => x.Key, x => x.Count());
+            return dict[1] * dict[3];
+        }
+
+        public int Solve(int part = 1)
+            => SolvePart1();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AoC20
         {
             int day = 10;
             int part = 1;
-            bool test = !false;
+            bool test = false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -110,7 +110,9 @@ namespace AoC20
         static string day10(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return "";
+            Day10.JoltConnector conn = new();
+            conn.ParseInput(lines);
+            return conn.Solve(part).ToString();
         }
     }
 }
