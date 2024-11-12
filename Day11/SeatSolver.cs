3618a12d04b3df9e3527ea2119bbce721ee28a7e
silentDoc <ramon78t@yahoo.es>
2024-11-12 01:16:05 +0100

    Day 11 - Refactor to reuse code

--- Day11/SeatSolver.cs
+++ Day11/SeatSolver.cs
@@ -21,57 +21,10 @@ namespace AoC20.Day11
 
         string GetState(Dictionary<Coord2D, char> seatMap)
             => string.Concat(seatMap.Values);
-               
-        Dictionary<Coord2D, char> Step(Dictionary<Coord2D, char> seatMap)
-        {
-            int maxX = SeatMap.Keys.Max(k => k.x);
-            int maxY = SeatMap.Keys.Max(k => k.y);
-            Dictionary<Coord2D, char> retVal = new();
-
-            foreach(var key in seatMap.Keys) 
-            {
-                if (seatMap[key] == Seat.Floor)
-                {
-                    retVal[key] = Seat.Floor;
-                    continue;
-                }
-
-                var neighs = key.GetNeighbors8().Where(n => n.x >= 0 && n.y >= 0 && n.y <= maxY && n.x <= maxX).ToList();
-                var occupied = neighs.Count(k => seatMap[k] == Seat.Full);
-
-                if (seatMap[key] == Seat.Empty && occupied == 0)
-                    retVal[key] = Seat.Full;
-                else if (seatMap[key] == Seat.Full && occupied >= 4)
-                    retVal[key] = Seat.Empty;
-                else
-                    retVal[key] = seatMap[key];
-            }
-
-            return retVal;
-        }
-    
-        private int SolvePart1()
-        {
-            string previousState = "";
-            var currentState = GetState(SeatMap);
-            Dictionary<Coord2D, char> nextStep = new();
-
-            foreach (var key in SeatMap.Keys)
-                nextStep[key] = SeatMap[key];
-
-            while (previousState != currentState)
-            {
-                nextStep = Step(nextStep);
-                previousState = currentState;
-                currentState = GetState(nextStep);
-            }
-
-            return nextStep.Values.Count(x => x == Seat.Full);
-        }
 
         List<Coord2D> GetVisibleNeighs(Coord2D key, Dictionary<Coord2D, char> seatMap)
         {
-            List<Coord2D> dirs = [(-1, 0), (1, 0), (0,-1), (0,1), (-1, -1), (1, -1), (-1, 1),(1, 1)];
+            List<Coord2D> dirs = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, -1), (-1, 1), (1, 1)];
             List<Coord2D> retVal = new();
 
             foreach (var dir in dirs)
@@ -85,13 +38,14 @@ namespace AoC20.Day11
             return retVal;
         }
 
-        Dictionary<Coord2D, char> StepPart2(Dictionary<Coord2D, char> seatMap)
+        Dictionary<Coord2D, char> Step(Dictionary<Coord2D, char> seatMap, int part = 1)
         {
             int maxX = SeatMap.Keys.Max(k => k.x);
             int maxY = SeatMap.Keys.Max(k => k.y);
             Dictionary<Coord2D, char> retVal = new();
+            int minOccupied = part == 1 ? 4 : 5;
 
-            foreach (var key in seatMap.Keys)
+            foreach(var key in seatMap.Keys) 
             {
                 if (seatMap[key] == Seat.Floor)
                 {
@@ -99,12 +53,14 @@ namespace AoC20.Day11
                     continue;
                 }
 
-                var neighs = GetVisibleNeighs(key, seatMap);
+                var neighs = part == 1 ? key.GetNeighbors8().Where(n => n.x >= 0 && n.y >= 0 && n.y <= maxY && n.x <= maxX).ToList()
+                                       : GetVisibleNeighs(key, seatMap); 
+
                 var occupied = neighs.Count(k => seatMap[k] == Seat.Full);
 
                 if (seatMap[key] == Seat.Empty && occupied == 0)
                     retVal[key] = Seat.Full;
-                else if (seatMap[key] == Seat.Full && occupied >= 5)
+                else if (seatMap[key] == Seat.Full && occupied >= minOccupied)
                     retVal[key] = Seat.Empty;
                 else
                     retVal[key] = seatMap[key];
@@ -113,7 +69,7 @@ namespace AoC20.Day11
             return retVal;
         }
 
-        private int SolvePart2()
+        private int SolveSeats(int part =1)
         {
             string previousState = "";
             var currentState = GetState(SeatMap);
@@ -124,7 +80,7 @@ namespace AoC20.Day11
 
             while (previousState != currentState)
             {
-                nextStep = StepPart2(nextStep);
+                nextStep = Step(nextStep, part);
                 previousState = currentState;
                 currentState = GetState(nextStep);
             }
@@ -133,6 +89,6 @@ namespace AoC20.Day11
         }
 
         public int Solve(int part = 1)
-            => part == 1 ? SolvePart1() : SolvePart2();
+            => SolveSeats(part);
     }
 }
