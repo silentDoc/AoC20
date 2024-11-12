7b38d9cee454e78b3b9fa2aa58d83266c3e85792
silentDoc <ramon78t@yahoo.es>
2024-11-12 01:07:44 +0100

    Day 11 - Part 2 Completed. New Rules

--- Day11/SeatSolver.cs
+++ Day11/SeatSolver.cs
@@ -69,7 +69,70 @@ namespace AoC20.Day11
             return nextStep.Values.Count(x => x == Seat.Full);
         }
 
+        List<Coord2D> GetVisibleNeighs(Coord2D key, Dictionary<Coord2D, char> seatMap)
+        {
+            List<Coord2D> dirs = [(-1, 0), (1, 0), (0,-1), (0,1), (-1, -1), (1, -1), (-1, 1),(1, 1)];
+            List<Coord2D> retVal = new();
+
+            foreach (var dir in dirs)
+            {
+                var current = key + dir;
+                while (seatMap.ContainsKey(current) && seatMap[current] == Seat.Floor)
+                    current += dir;
+                if (seatMap.ContainsKey(current))
+                    retVal.Add(current);
+            }
+            return retVal;
+        }
+
+        Dictionary<Coord2D, char> StepPart2(Dictionary<Coord2D, char> seatMap)
+        {
+            int maxX = SeatMap.Keys.Max(k => k.x);
+            int maxY = SeatMap.Keys.Max(k => k.y);
+            Dictionary<Coord2D, char> retVal = new();
+
+            foreach (var key in seatMap.Keys)
+            {
+                if (seatMap[key] == Seat.Floor)
+                {
+                    retVal[key] = Seat.Floor;
+                    continue;
+                }
+
+                var neighs = GetVisibleNeighs(key, seatMap);
+                var occupied = neighs.Count(k => seatMap[k] == Seat.Full);
+
+                if (seatMap[key] == Seat.Empty && occupied == 0)
+                    retVal[key] = Seat.Full;
+                else if (seatMap[key] == Seat.Full && occupied >= 5)
+                    retVal[key] = Seat.Empty;
+                else
+                    retVal[key] = seatMap[key];
+            }
+
+            return retVal;
+        }
+
+        private int SolvePart2()
+        {
+            string previousState = "";
+            var currentState = GetState(SeatMap);
+            Dictionary<Coord2D, char> nextStep = new();
+
+            foreach (var key in SeatMap.Keys)
+                nextStep[key] = SeatMap[key];
+
+            while (previousState != currentState)
+            {
+                nextStep = StepPart2(nextStep);
+                previousState = currentState;
+                currentState = GetState(nextStep);
+            }
+
+            return nextStep.Values.Count(x => x == Seat.Full);
+        }
+
         public int Solve(int part = 1)
-            => SolvePart1();
+            => part == 1 ? SolvePart1() : SolvePart2();
     }
 }
