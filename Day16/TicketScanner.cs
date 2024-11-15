6d32b2f19f99df399c0b9afc35f8fcab6929003b
silentDoc <ramon78t@yahoo.es>
2024-11-15 08:28:39 +0100

    Day 16 - Part 2 Completed. Common elements

--- Day16/TicketScanner.cs
+++ Day16/TicketScanner.cs
@@ -40,17 +40,57 @@ namespace AoC20.Day16
                 nearbyTickets.Add(ticket.Split(",").Select(int.Parse).ToList());
         }
 
-        int SolvePart1()
+        long SolvePart1()
         {
             int retVal = 0;
 
             foreach (var ticket in nearbyTickets)
                 retVal += ticket.Where(x => !rules.Any(r => r.IsValid(x))).Sum();
 
+            return (long) retVal;
+        }
+
+        bool IsValid(List<int> ticket)
+           => ticket.All(x => rules.Any(r => r.IsValid(x)));
+
+        List<string> ValidRules(int value)
+            => rules.Where(r => r.IsValid(value)).Select(x => x.Name).ToList();
+
+        long SolvePart2()
+        {
+            Dictionary<int, List<string>> commonElements = new();
+            Dictionary<int, string> ticketFields = new();
+
+            var validTickets = nearbyTickets.Where(x => IsValid(x)).Prepend(myTicket).ToList();
+
+            for (int i = 0; i < myTicket.Count; i++)
+                commonElements[i] = rules.Select(x => x.Name).ToList();
+
+            foreach (var ticket in validTickets)
+                for (int i = 0; i < myTicket.Count; i++)
+                    commonElements[i] = commonElements[i].Intersect(ValidRules(ticket[i])).ToList();
+
+            for (int i = 0; i < myTicket.Count; i++)
+            {
+                var singleIndex = commonElements.Keys.First(x => commonElements[x].Count() == 1);
+                var element = commonElements[singleIndex][0];
+                ticketFields[singleIndex] = element;
+
+                foreach (var fields in commonElements.Values)
+                    if (fields.Contains(element))
+                        fields.Remove(element);
+            }
+
+            var retIndices = ticketFields.Keys.Where(x => ticketFields[x].StartsWith("departure")).ToList();
+
+            long retVal = 1;
+            foreach (var pos in retIndices)
+                retVal *= (long)myTicket[pos];
+
             return retVal;
         }
 
-        public int Solve(int part = 1)
-            => SolvePart1();
+        public long Solve(int part = 1)
+            => part ==1 ? SolvePart1() : SolvePart2();
     }
 }
