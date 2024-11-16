210d3e398e5757fddc7a81555bd951599f9cb52a
silentDoc <ramon78t@yahoo.es>
2024-11-16 18:40:02 +0100

    Day 18 - Part 2 Completed. Advanced math mode

--- Day18/MathParser.cs
+++ Day18/MathParser.cs
@@ -7,7 +7,7 @@
         public MathExpression(string expr)
             => Expr = expr;
 
-        public long Solve()
+        public long Solve(bool advancedMode = false)
         {
             long res = 0;
             char op = '_';
@@ -20,7 +20,7 @@
                 if (Expr[i] == '(')
                 {
                     var level = 1;
-                    var pos = i+1;
+                    var pos = i + 1;
                     while (level != 0)
                     {
                         if (Expr[pos] == '(')
@@ -31,9 +31,9 @@
                     }
 
                     var subExpression = Expr.Substring(i + 1, pos - (i + 1));
-                    var subResult = new MathExpression(subExpression).Solve();
+                    var subResult = new MathExpression(subExpression).Solve(advancedMode);
 
-                    res = (op == '_') ? subResult : ( (op == '+') ? res + subResult : res * subResult);
+                    res = (op == '_') ? subResult : ((op == '+') ? res + subResult : res * subResult);
                     i = pos;
                     continue;
                 }
@@ -44,10 +44,20 @@
                     res = (op == '_') ? number : ((op == '+') ? res + number : res * number);
                 }
 
-                if (Expr[i] == '*' || Expr[i] == '+')
+                if (Expr[i] == '+')
                     op = Expr[i];
+
+                if (Expr[i] == '*' && !advancedMode)
+                    op = Expr[i];
+
+                if (Expr[i] == '*' && advancedMode)
+                {
+                    var subExpr = Expr.Substring(i + 1, Expr.Length - (i + 1));
+                    return res * new MathExpression(subExpr).Solve(advancedMode);
+                }
+
             }
-            
+
             return res;
         }
     }
@@ -59,7 +69,6 @@
             => lines.ForEach(x => expressions.Add(new MathExpression(x)));
 
         public long Solve(int part = 1)
-            => expressions.Sum(x => x.Solve());
-
+            => expressions.Sum(x => x.Solve(part == 2));
     }
 }
