608f3259102396f937f0309ef08fbd222040e3c0
silentDoc <ramon78t@yahoo.es>
2024-11-25 12:46:00 +0100

    Day 22 - Part 1 Completed. Simple CardGame

---
+++ Day22/CardGame.cs
@@ -0,0 +1,39 @@
+﻿using AoC20.Common;
+
+namespace AoC20.Day22
+{
+    internal class CardGame
+    {
+        List<int> deck1 = [];
+        List<int> deck2 = [];
+
+        public void ParseInput(List<string> input)
+        {
+            var decks = ParseUtils.SplitBy(input, "");
+            deck1 = decks[0][1..].Select(x => int.Parse(x)).ToList();
+            deck2 = decks[1][1..].Select(x => int.Parse(x)).ToList();
+        }
+
+        int SolvePart1()
+        {
+            while (deck1.Any() && deck2.Any())
+            {
+                if(deck1[0] > deck2[0])
+                    deck1.AddRange([deck1[0] , deck2[0]]);
+                else
+                    deck2.AddRange([deck2[0], deck1[0]]);
+
+                deck1.RemoveAt(0);
+                deck2.RemoveAt(0);
+            }
+
+            var winning = deck1.Any() ? deck1 : deck2;
+            var scores = Enumerable.Range(1, winning.Count).Reverse().ToList();
+
+            return scores.Zip(winning).Select(el => el.First * el.Second).Sum();   
+        }
+
+        public int Solve(int part = 1)
+            => SolvePart1();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AoC20
         {
             int day = 22;
             int part = 1;
-            bool test = !false;
+            bool test = false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -219,9 +219,9 @@ namespace AoC20
         static string day22(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            //Day21.AllergenCheck checker = new();
-            //checker.ParseInput(lines);
-            return "";// checker.Solve(part).ToString();
+            Day22.CardGame game = new();
+            game.ParseInput(lines);
+            return game.Solve(part).ToString();
         }
     }
 }
